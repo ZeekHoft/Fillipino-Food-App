dff21677fad34623b819291c2305037d365164b0
Nero Hoya <dltposiedon@gmail.com>
2025-03-07 23:02:20 +0800

    setting up image picker halfway done, need to produce link

---
+++ lib/pages/image_picker.dart
@@ -0,0 +1,55 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flilipino_food_app/util/services.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class Image_Picker extends StatefulWidget {
+  const Image_Picker({super.key});
+
+  @override
+  State<Image_Picker> createState() => _Image_PickerState();
+}
+
+class _Image_PickerState extends State<Image_Picker> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                onPressed: () async {
+                  final ImagePicker picker = ImagePicker();
+                  final XFile? image =
+                      await picker.pickImage(source: ImageSource.camera);
+                  if (image != null) {
+                    Uint8List bytes = await image.readAsBytes();
+                    UploadApiImage()
+                        .uploadImage(bytes, image.name)
+                        .then((value) {
+                      setState(() {});
+                      print(
+                          "uplaode successfully eto link par: ${value.toString()}");
+                    }).onError(
+                      (error, stackTrace) {
+                        print(error.toString());
+                      },
+                    );
+                  }
+                },
+                child: const Text(
+                  "Upload Image",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.white),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
