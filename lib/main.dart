32934281ce145993700e970f3e135d1851eaa54f
Nero Hoya <dltposiedon@gmail.com>
2025-03-21 22:46:49 +0800

    setting up image recognition for openAI

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,13 @@ import 'package:flilipino_food_app/pages/search_recipe.dart';
 import 'package:flilipino_food_app/util/recipe_stream_builder.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // await dotenv.load(fileName: ".env");
+  await dotenv.load(fileName: ".env");
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
