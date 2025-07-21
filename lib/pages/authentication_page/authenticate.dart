65edb12905a71f463a5ee0848c498f6fe100c62b
h1tmd <105845103+h1tmd@users.noreply.github.com>
2025-07-21 23:02:45 +0800

    Remove unused imports

--- lib/pages/authentication_page/authenticate.dart
+++ lib/pages/authentication_page/authenticate.dart
@@ -1,9 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flilipino_food_app/pages/authentication_page/registered_or_login.dart';
-import 'package:flilipino_food_app/pages/authentication_page/signup_page.dart';
-import 'package:flilipino_food_app/pages/authentication_page/user_input.dart';
 import 'package:flilipino_food_app/pages/home_page/home_layout.dart';
-import 'package:flilipino_food_app/pages/home_page/home_widgets/profile_section.dart';
 import 'package:flutter/material.dart';
 
 class Authenticate extends StatelessWidget {
