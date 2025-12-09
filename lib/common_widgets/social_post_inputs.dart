b13c2629851bf54e4ed17c9877d84fada78ad7c4
h1tmd <105845103+h1tmd@users.noreply.github.com>
2025-12-09 23:14:03 +0800

    Add label text and hint text

--- lib/common_widgets/social_post_inputs.dart
+++ lib/common_widgets/social_post_inputs.dart
@@ -1,44 +1,58 @@
 import 'package:flutter/material.dart';
 
 class SocialPostInputs extends StatelessWidget {
-  final int maxLines;
+  final int? maxLines;
   final String labelText;
   final String errorText;
+  final String? hintText;
   final TextInputType keyboardType;
   final TextEditingController controller;
 
-  const SocialPostInputs({
-    super.key,
-    required this.controller,
-    required this.keyboardType,
-    required this.labelText,
-    required this.errorText,
-    this.maxLines = 3,
-  });
+  const SocialPostInputs(
+      {super.key,
+      required this.controller,
+      required this.keyboardType,
+      required this.labelText,
+      required this.errorText,
+      this.maxLines = 3,
+      this.hintText});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: TextFormField(
-        controller: controller,
-        keyboardType: keyboardType,
-        maxLines: maxLines,
-        decoration: InputDecoration(
-          hintText: labelText,
-          border: OutlineInputBorder(
-            borderSide: BorderSide.none,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(
+            left: 8.0,
+            bottom: 4.0,
+          ),
+          child: Text(labelText, style: Theme.of(context).textTheme.bodyLarge),
+        ),
+        Card(
+          margin: EdgeInsets.zero,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: TextFormField(
+            controller: controller,
+            keyboardType: keyboardType,
+            maxLines: maxLines,
+            decoration: InputDecoration(
+              hintText: hintText,
+              border: OutlineInputBorder(
+                borderSide: BorderSide.none,
+              ),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return errorText;
+              }
+              return null;
+            },
           ),
         ),
-        validator: (value) {
-          if (value == null || value.isEmpty) {
-            return errorText;
-          }
-          return null;
-        },
-      ),
+      ],
     );
   }
 }
