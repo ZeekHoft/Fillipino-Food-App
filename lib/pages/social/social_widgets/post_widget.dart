b406d1cdcda85c546c552c40bfa0b6e396d6c52b
h1tmd <105845103+h1tmd@users.noreply.github.com>
2025-12-10 14:54:39 +0800

    Move text description into outside column widget so that it extends to the whole width

--- lib/pages/social/social_widgets/post_widget.dart
+++ lib/pages/social/social_widgets/post_widget.dart
@@ -52,7 +52,9 @@ class _PostWidgetState extends State<PostWidget> {
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              // Top part of post
               Row(
                 children: [
                   CircleAvatar(),
@@ -82,135 +84,126 @@ class _PostWidgetState extends State<PostWidget> {
                 ],
               ),
               const SizedBox(height: 8.0),
-              IntrinsicHeight(
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          // Picture Here
-                          Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(4),
-                              color: Theme.of(context)
-                                  .colorScheme
-                                  .surfaceContainerHighest,
-                            ),
-                            height: 300,
-                            clipBehavior: Clip.antiAlias,
-                            child: widget.post["postPic"] != null ||
-                                    widget.post["postPic"] != ""
-                                ? Image.asset(
-                                    "assets/dappli_logo.png",
-                                    fit: BoxFit.cover,
-                                  )
-                                // Image.network(
-                                //     widget.post["postPic"],
-                                //     width: 50,
-                                //     height: 50,
-                                //     fit: BoxFit.fitWidth,
-                                //   )
-                                : const Icon(Icons.image_not_supported),
-                          ),
-                          const SizedBox(height: 8.0),
-                          Text(
-                            description,
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                              "${formatter.format(double.parse(calories))} calories"),
-                          SizedBox(height: 4),
-                          if (ingredients.isNotEmpty) ...[
-                            Text(
-                              "Ingredients: ${ingredients.join(", ")}",
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                            )
-                          ],
-                          SizedBox(height: 4),
-                          if (processSteps.isNotEmpty) ...[
-                            Text(
-                              "Process: ${processSteps.join(", ")}",
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ],
-                        ],
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(4),
+                        color: Theme.of(context)
+                            .colorScheme
+                            .surfaceContainerHighest,
                       ),
+                      height: 300,
+                      clipBehavior: Clip.antiAlias,
+                      child: widget.post["postPic"] != null ||
+                              widget.post["postPic"] != ""
+                          ? Image.asset(
+                              "assets/dappli_logo.png",
+                              fit: BoxFit.cover,
+                            )
+                          // Image.network(
+                          //     widget.post["postPic"],
+                          //     width: 50,
+                          //     height: 50,
+                          //     fit: BoxFit.fitWidth,
+                          //   )
+                          : const Icon(Icons.image_not_supported),
                     ),
-                    const SizedBox(width: 8.0),
-                    Column(
-                      // crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        // Like Button
-                        LikeButton(
-                          postId: postId,
-                          userId: currentUserId!,
-                          likedAccounts: widget.post["likedAccounts"],
+                  ),
+                  const SizedBox(width: 8.0),
+                  Column(
+                    children: [
+                      // Like Button
+                      LikeButton(
+                        postId: postId,
+                        userId: currentUserId!,
+                        likedAccounts: widget.post["likedAccounts"],
+                      ),
+
+                      const SizedBox(height: 8.0),
+
+                      SaveButton(post: widget.post, provider: provider),
+
+                      const SizedBox(height: 8.0),
+
+                      // Share Button (unimplemented)
+                      IconButton(onPressed: () {}, icon: Icon(Icons.share)),
+
+                      const SizedBox(height: 8.0),
+
+                      // THIS NEEDS TO BE REVIEWED FOR POTENTIAL ERRORS
+                      // Delete Post Button
+                      if (isPostOwner)
+                        IconButton(
+                          onPressed: () async {
+                            // Confirmation dialog is highly recommended here!
+                            final confirm = await showDialog<bool>(
+                              context: context,
+                              builder: (ctx) => AlertDialog(
+                                title: const Text('Delete Post'),
+                                content: const Text(
+                                    'Are you sure you want to delete this post? This cannot be undone.'),
+                                actions: <Widget>[
+                                  TextButton(
+                                    child: const Text('Cancel'),
+                                    onPressed: () =>
+                                        Navigator.of(ctx).pop(false),
+                                  ),
+                                  TextButton(
+                                    child: Text(
+                                      'Delete',
+                                      style: TextStyle(
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .error),
+                                    ),
+                                    onPressed: () =>
+                                        Navigator.of(ctx).pop(true),
+                                  ),
+                                ],
+                              ),
+                            );
+
+                            if (confirm == true) {
+                              // Call the delete function from the provider
+                              await socialDataStoring.deletePost(
+                                  postId!, favoriteProvider);
+                            }
+                          },
+                          icon: Icon(Icons.delete,
+                              color: Theme.of(context).colorScheme.error),
                         ),
-
-                        const SizedBox(height: 8.0),
-
-                        SaveButton(post: widget.post, provider: provider),
-
-                        const SizedBox(height: 8.0),
-
-                        // Share Button (unimplemented)
-                        IconButton(onPressed: () {}, icon: Icon(Icons.share)),
-
-                        const SizedBox(height: 8.0),
-
-                        // THIS NEEDS TO BE REVIEWED FOR POTENTIAL ERRORS
-                        // Delete Post Button
-                        if (isPostOwner)
-                          IconButton(
-                            onPressed: () async {
-                              // Confirmation dialog is highly recommended here!
-                              final confirm = await showDialog<bool>(
-                                context: context,
-                                builder: (ctx) => AlertDialog(
-                                  title: const Text('Delete Post'),
-                                  content: const Text(
-                                      'Are you sure you want to delete this post? This cannot be undone.'),
-                                  actions: <Widget>[
-                                    TextButton(
-                                      child: const Text('Cancel'),
-                                      onPressed: () =>
-                                          Navigator.of(ctx).pop(false),
-                                    ),
-                                    TextButton(
-                                      child: Text(
-                                        'Delete',
-                                        style: TextStyle(
-                                            color: Theme.of(context)
-                                                .colorScheme
-                                                .error),
-                                      ),
-                                      onPressed: () =>
-                                          Navigator.of(ctx).pop(true),
-                                    ),
-                                  ],
-                                ),
-                              );
-
-                              if (confirm == true) {
-                                // Call the delete function from the provider
-                                await socialDataStoring.deletePost(
-                                    postId!, favoriteProvider);
-                              }
-                            },
-                            icon: Icon(Icons.delete,
-                                color: Theme.of(context).colorScheme.error),
-                          ),
-                        // THIS NEEDS TO BE REVIEWED FOR POTENTIAL ERRORS
-                      ],
-                    ),
-                  ],
+                      // THIS NEEDS TO BE REVIEWED FOR POTENTIAL ERRORS
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(height: 8.0),
+              Text(
+                description,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
                 ),
               ),
+              Text("${formatter.format(double.parse(calories))} calories"),
+              SizedBox(height: 4),
+              if (ingredients.isNotEmpty) ...[
+                Text(
+                  "Ingredients: ${ingredients.join(", ")}",
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                )
+              ],
+              SizedBox(height: 4),
+              if (processSteps.isNotEmpty) ...[
+                Text(
+                  "Process: ${processSteps.join(", ")}",
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
             ],
           ),
         ),
