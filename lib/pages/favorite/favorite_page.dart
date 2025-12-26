9737d323d06b4aee3f4675b2f76bbec7afbcc70a
h1tmd <105845103+h1tmd@users.noreply.github.com>
2025-12-26 20:27:55 +0800

    Correct misspelled recipe provider variable

--- lib/pages/favorite/favorite_page.dart
+++ lib/pages/favorite/favorite_page.dart
@@ -35,17 +35,17 @@ class _FavoritePageState extends State<FavoritePage>
 
   @override
   Widget build(BuildContext context) {
-    final recipProvider = Provider.of<FavoriteProvider>(context);
+    final recipeProvider = Provider.of<FavoriteProvider>(context);
     final socialProvider = Provider.of<FavoriteSocialProvider>(context);
 
     // Now, these lists are populated by the loadFavorites() method after fetching from Firestore
-    final recipeNames = recipProvider.recipeNames;
-    final favoriteRecipeIds = recipProvider.recipeFavoriteId;
+    final recipeNames = recipeProvider.recipeNames;
+    final favoriteRecipeIds = recipeProvider.recipeFavoriteId;
 
-    final recipeImages = recipProvider.recipeImages;
-    final recipeCalories = recipProvider.recipeCalories;
-    final List<dynamic> recipeIngredients = recipProvider.recipeIngredients;
-    final List<dynamic> recipeProcesses = recipProvider.recipeProcess;
+    final recipeImages = recipeProvider.recipeImages;
+    final recipeCalories = recipeProvider.recipeCalories;
+    final List<dynamic> recipeIngredients = recipeProvider.recipeIngredients;
+    final List<dynamic> recipeProcesses = recipeProvider.recipeProcess;
     // Get the list of IDs
     final socialPostFavorites = socialProvider.favoritePost;
 
@@ -56,7 +56,7 @@ class _FavoritePageState extends State<FavoritePage>
     final bool allFavoritesEmpty =
         favoriteRecipeIds.isEmpty && socialPostFavorites.isEmpty;
 
-    if (recipProvider.isLoading || socialProvider.isLoading) {
+    if (recipeProvider.isLoading || socialProvider.isLoading) {
       return const Center(
           child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -105,7 +105,7 @@ class _FavoritePageState extends State<FavoritePage>
             child: TabBarView(
               controller: _tabController,
               children: [
-                FavoriteRecipesList(recipeProvider: recipProvider),
+                FavoriteRecipesList(recipeProvider: recipeProvider),
                 FavoritePostsList(socialProvider: socialProvider),
               ],
             ),
